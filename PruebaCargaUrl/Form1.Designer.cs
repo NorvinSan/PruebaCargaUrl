df8d789594e72e60a410c33ad227013bf5cca594
norvin Gutierrez <norvinantoniog@gmail.com>
2022-02-28 10:05:11 -0600

    Add project files.

---
+++ PruebaCargaUrl/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace PruebaCargaUrl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.btnCargar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Location = new System.Drawing.Point(47, 49);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.ShadowDecoration.Parent = this.guna2PictureBox1;
+            this.guna2PictureBox1.Size = new System.Drawing.Size(335, 247);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox1.TabIndex = 0;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(175, 311);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(90, 27);
+            this.btnCargar.TabIndex = 1;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 362);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private System.Windows.Forms.Button btnCargar;
+    }
+}
+
---
+++ PruebaCargaUrl/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PruebaCargaUrl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //"https://cdn4.josefacchin.com/wp-content/uploads/2020/02/como-quitar-el-fondo-de-una-imagen.png"
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            Stream StreamImagen;
+            string elError = "";
+            StreamImagen = getUrl("");
+            if (elError == "")
+            {
+                guna2PictureBox1.Image = System.Drawing.Image.FromStream(StreamImagen);
+            }
+            else
+            {
+                //MsgBox(elError);
+            }
+
+        }
+        // do it 
+        private static Stream getUrl(string URL)
+        {
+            URL = "https://lh3.googleusercontent.com/e40R8CokfUFCwx_hWTRJNKXSClNQxAi6Ua3eb8PYlrRUmnpmSlPjLBl4Tc0eLa0352H12xfIOV58WCQpOAQPhBAm8vczxG8EPmchkGWNRLTW9xcwmQ_-uJtaq5SXrw990cnh7vC637_HYYFlt4khKURrLfs8ZpB-yf81FduhgsBkzxPgcZ5wEuA2gDCuYkmxgux2ETxaGxupilx9GmGgl84BFppAQzj35lP4yFSd89iVmIupb4AztE5L4EEqmlIlp-69Qgy9oaQKpb1AHK3cBPcTmL9TzWJjkBTgkZxpFCL6juy2WcK6lHVrzhChNbTUXxCl0ZHDmFvKu84b3A80lTWBK2HZDVtElZbLsFHdfAZ0ohOu_6Q1A23UA6D4G6y0E0YWhYC3O-UG45n2COg3L6b_0e1yL4_O28j_svEPXPgTq_-t0GBFzwvT_uMdbHDQitD1H7L0U_lb-tEboSuOcWQwopSWcmaELsfsgbM2IRtL1Eyp7doOo58vgsYcfE5PkpLBTuNmaODt2P8m31VbIxa7JHZ5rqj26q_3aq2sFTIkTBEVoJAeDXB1FUWa4rt5vUyLjVPeIl0MlRi-FgJG779NdWsA9qKzvw4eAmYGtLjGM6IezYcaQaF1QEut0ndk8UzMRpyixtkcqMyu2U0aBJJw03bDAHWg1vrwgT8cRc8Uuk1286VnMwxWlRvbSM4cE50qNQ2-oEh82glHuTMA-gbv=w1295-h969-no?authuser=1";
+           // string strResp = "https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen.jpg";
+            HttpWebRequest request = ((HttpWebRequest)WebRequest.Create(URL));
+
+            HttpWebResponse response = ((HttpWebResponse)request.GetResponse());
+
+            try
+            {
+
+                return response.GetResponseStream();
+
+            }
+            catch
+            {
+                return response.GetResponseStream();
+                //elError = ex.ToString();
+            }
+
+
+            /*private static void UploadBasicImage(string path, DriveService service)
+            {
+                var fileMetadata = new Google.Apis.Drive.v3.Data.File();
+                fileMetadata.Name = Path.GetFileName(path);
+                fileMetadata.MimeType = "image/jpeg";
+                FilesResource.CreateMediaUpload request;
+                using (var stream = new System.IO.FileStream(path, System.IO.FileMode.Open))
+                {
+                    request = service.Files.Create(fileMetadata, stream, "image/jpeg");
+                    request.Fields = "id";
+                    request.Upload();
+                }
+
+                var file = request.ResponseBody;
+
+                Console.WriteLine("File ID: " + file.Id);
+
+            }*/
+
+        }
+    }
+}  
