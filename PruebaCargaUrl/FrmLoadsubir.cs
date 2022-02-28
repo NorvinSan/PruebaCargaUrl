0eba545b8274d1247263686a3746a09910017f72
kevin cardoza <pevin.kevin@gmail.com>
2022-02-28 16:25:13 -0600

    cambios finales cloudbinary si funciona alv

--- PruebaCargaUrl/FrmLoadsubir.cs
+++ PruebaCargaUrl/FrmLoadsubir.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using CloudinaryDotNet.Core;
+using CloudinaryDotNet.Provisioning;
+
 
 namespace PruebaCargaUrl
 {
@@ -16,13 +21,51 @@ namespace PruebaCargaUrl
         public FrmLoadsubir()
         {
             InitializeComponent();
+            cloud = new Cloudinary(A); 
+            
         }
+
+        public static Cloudinary cloud;
+        Account A = new Account("pandaop", "632968835138977", "GDDZ6eYUYF5_upckyKxZqbpQm8w");
+
+        private void CargarImagen(String ruta) {
+            try
+            {
+                var uploadParams = new ImageUploadParams()
+                {
+                    File = new FileDescription(ruta),
+                };
+
+                var uploadResult = cloud.Upload(uploadParams);
+
+                ruta = uploadResult.SecureUri.ToString();
+                MessageBox.Show("Foto subida correctamente al servidor cloudinary");
+
+                ptbConfrirm.ImageLocation = @"" + ruta;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        
+        
+        
+        }
+        
 
         private void btnload_Click(object sender, EventArgs e)
         {
-
+            if (imagen !="")
+            {
+                CargarImagen(imagen);
+            }
+            else
+            {
+                MessageBox.Show("pegate un tiro mejor");
+            }
         }
-
+     
         private void ptbLoad_Click(object sender, EventArgs e)
         {
             if (ofdSelectorImagen.ShowDialog() == System.Windows.Forms.DialogResult.OK)
